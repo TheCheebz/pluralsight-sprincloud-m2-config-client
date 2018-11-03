ddb62482f23ba7dc46eb3381da792134bcd6ffdc
Cheebz <thecheebz@gmail.com>
2018-11-03 10:55:55 -0400

    Initial Commit

---
+++ src/main/java/pluralsight/demo/PluralsightSprincloudM2ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSprincloudM2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSprincloudM2ConfigClientApplication.class, args);
+	}
+}
