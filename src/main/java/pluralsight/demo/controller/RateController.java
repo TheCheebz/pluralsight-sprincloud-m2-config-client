47b92244189e2049e469562075906c8d3fc7eb95
Cheebz <thecheebz@gmail.com>
2018-11-03 12:00:08 -0400

    added client side decryption

--- src/main/java/pluralsight/demo/controller/RateController.java
+++ src/main/java/pluralsight/demo/controller/RateController.java
@@ -1,11 +1,13 @@
 package pluralsight.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 	
 	@Value("${rate}")
@@ -14,12 +16,15 @@ public class RateController {
 	String laneCount;
 	@Value("${tollstart}")
 	String tollStart;
+	@Value("${connstring}")
+	String connString;
 	
 	@RequestMapping("/rate")
 	public String getRate(Model m) {
 		m.addAttribute("rateamount", rate);
 		m.addAttribute("lanes",laneCount);
 		m.addAttribute("tollstart",tollStart);
+		m.addAttribute("connstring",connString);
 		
 		return "rateview";
 	}
